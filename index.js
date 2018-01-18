3b9e489d7b202437ab375dd9ba80d78fd960a77f
Xochitl Menjivar <xandra.menji@gmail.com>
2018-01-18 00:39:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians, instruments){
   var theBand = [];
   
   for(var x = 0; x < musicians.length; x++){
-    
+    theBand[x] = musicians[x] + " plays " + instruments[x];
   }
-  
+  return theBand;
 }
